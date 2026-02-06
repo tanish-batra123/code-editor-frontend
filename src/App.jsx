b59dc9f23c28aa0bb1bf1f061383f2e3dfb532de
tanish-batra123 <tanishbatra893@gmail.com>
2026-02-05 20:41:46 -0800

    fix: case sensitive import Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { EditorPage } from "./pages/EditorPage";
 import { Toaster } from "react-hot-toast";
 
